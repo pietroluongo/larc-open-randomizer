43f1150c086458ef28f6307b1479743cac48e5a1
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2022-10-16 15:59:41 -0300

    Random cube generation implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import CubeGrid from './components/CubeGrid'
 import CubeGridContainer from './components/CubeGridContainer'
@@ -20,20 +19,152 @@ const shouldHighlight = ({ line, col }: HighlightProps) => {
   return (col + 1 == 2 || col + 1 == 3) && (line + 1 == 3 || line + 1 == 4) || ((col + 1 == 5 || col + 1 == 6) && (line + 1 == 3 || line + 1 == 4))
 }
 
+interface CubeProps {
+  line: number;
+  col: number;
+  type?: "loadingZone" | "dropZone" | "none";
+  value?: string;
+}
+
+// Sorteia 12 cubos:
+// 4 são de cores [Verde, Azul, Vermelho, Amarelo]
+// 4 são de letras (a..i)
+// 4 são de QR Code (1..9)
+
+
+const sortLetters = (): string[] => {
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+  const shuffled = letters.sort(() => 0.5 - Math.random())
+  return shuffled.slice(0, 4)
+}
+
+const sortQrCodes = (): number[] => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const shuffled = numbers.sort(() => 0.5 - Math.random())
+  return shuffled.slice(0, 4)
+}
+
+interface LettersWithPosition {
+  letter: string;
+  coords: CompleteCoordinates;
+}
+
+interface QRWithPosition {
+  code: number;
+  coords: CompleteCoordinates;
+}
+
+type CubeColor = "green" | "blue" | "red" | "yellow"
+
+interface ColorWithPosition {
+  color: CubeColor;
+  coords: CompleteCoordinates;
+}
+
+interface CompleteCoordinates {
+  x: number;
+  y: number;
+  subX: number;
+  subY: number;
+}
+
+const sortLetterPositions = (usedPositions: CompleteCoordinates[]): LettersWithPosition[] => {
+  const letters = sortLetters()
+
+  return letters.map(letter => {
+    const generateCoordinate = (): CompleteCoordinates => {
+      const positionX = Math.floor(Math.random() * (4 - 3 + 1) + 3)
+      const positionY = Math.floor(Math.random() * (4 - 2 + 1) + 2)
+      const subX = Math.floor(Math.random() * (1 - 0 + 1) + 0)
+      const subY = Math.floor(Math.random() * (1 - 0 + 1) + 0)
+      return { x: positionX, y: positionY, subX, subY }
+    }
+
+    while (true) {
+      const coordinate = generateCoordinate()
+      if (!usedPositions.find(pos => pos.x === coordinate.x && pos.y === coordinate.y && pos.subX === coordinate.subX && pos.subY === coordinate.subY)) {
+        usedPositions.push(coordinate)
+        return { letter, coords: coordinate }
+      }
+    }
+  })
+}
+
+const sortQrPosition = (usedPositions: CompleteCoordinates[]): QRWithPosition[] => {
+  const qrs = sortQrCodes()
+
+  return qrs.map(qr => {
+    const generateCoordinate = (): CompleteCoordinates => {
+      const positionX = Math.floor(Math.random() * (4 - 3 + 1) + 3)
+      const positionY = Math.floor(Math.random() * (4 - 2 + 1) + 2)
+      const subX = Math.floor(Math.random() * (1 - 0 + 1) + 0)
+      const subY = Math.floor(Math.random() * (1 - 0 + 1) + 0)
+      return { x: positionX, y: positionY, subX, subY }
+    }
+
+    while (true) {
+      const coordinate = generateCoordinate()
+      if (!usedPositions.find(pos => pos.x === coordinate.x && pos.y === coordinate.y && pos.subX === coordinate.subX && pos.subY === coordinate.subY)) {
+        usedPositions.push(coordinate)
+        return { code: qr, coords: coordinate }
+      }
+    }
+  })
+}
+
+
+const sortColoredCubes = (usedPositions: CompleteCoordinates[]): ColorWithPosition[] => {
+  const colors: CubeColor[] = ["green", "blue", "red", "yellow"]
+
+  return colors.map(color => {
+    const generateCoordinate = (): CompleteCoordinates => {
+      const positionX = Math.floor(Math.random() * (4 - 3 + 1) + 3)
+      const positionY = Math.floor(Math.random() * (4 - 2 + 1) + 2)
+      const subX = Math.floor(Math.random() * (1 - 0 + 1) + 0)
+      const subY = Math.floor(Math.random() * (1 - 0 + 1) + 0)
+      return { x: positionX, y: positionY, subX, subY }
+    }
+
+    while (true) {
+      const coordinate = generateCoordinate()
+      if (!usedPositions.find(pos => pos.x === coordinate.x && pos.y === coordinate.y && pos.subX === coordinate.subX && pos.subY === coordinate.subY)) {
+        usedPositions.push(coordinate)
+        return { color, coords: coordinate }
+      }
+    }
+  })
+}
+
+const sortBlocksPositions = () => {
+  let usedCoords: CompleteCoordinates[] = []
+
+  const letters = sortLetterPositions(usedCoords)
+  const qrs = sortQrPosition(usedCoords)
+  const colors = sortColoredCubes(usedCoords)
+  return [...letters, ...qrs, ...colors]
+
+}
+
 const App = () => {
   return (
     <div className="App">
-      <h1>LARC - OPEN</h1>
+      <div className="m-10">
+        <h1>LARC - OPEN</h1>
+      </div>
       <CubeGridContainer>
         {CubeGridData().map((cubeGrid) => (
           cubeGrid.map(cubeItem => {
             if (shouldHighlight(cubeItem)) {
-              return <CubeGrid line={cubeItem.line + 1} col={cubeItem.col + 1} type="loadingZone" />
+              return <CubeGrid line={cubeItem.line} col={cubeItem.col} type="loadingZone" />
             }
-            return <CubeGrid line={cubeItem.line + 1} col={cubeItem.col + 1} />
+            return <CubeGrid line={cubeItem.line} col={cubeItem.col} />
           })
         ))}
       </CubeGridContainer>
+      <div className="m-10">
+        <button onClick={() => console.log(sortBlocksPositions())}>Randomize</button>
+      </div>
+
     </div>
   )
 }
--- src/components/CubeGrid/index.tsx
+++ src/components/CubeGrid/index.tsx
@@ -21,17 +21,17 @@ const CubeGrid: React.FC<Props> = ({ line, col, type = "none" }) => {
     if (type === "loadingZone") {
         return (
             <div className={`border border-blue w-[5rem] h-[5rem] flex grid grid-cols-2`}>
-                <div className="bg-blue-500 w-full h-full" />
+                {/* <div className="bg-blue-500 w-full h-full flex items-center justify-center"></div>
                 <div className="bg-green-500 w-full h-full" />
-                <div className="bg-pink-500 w-full h-full" />
-                <div className="bg-teal-500 w-full h-full" />
+                <div className="bg-pink-500 w-full h-full flex items-center justify-center"></div>
+                <div className="bg-teal-500 w-full h-full" /> */}
             </div>
         )
     }
 
     return (
         <div className={`border border-blue-500 p-3 w-[5rem] h-[5rem] flex items-center justify-center ${typeClassName()}`}>
-            ({line},{col})
+            ({line + 1},{col + 1})
         </div>
     )
 }
